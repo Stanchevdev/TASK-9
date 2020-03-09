246e425f03ffd0c8bc1c078ba3c097a489b2cfc0
Dimitar <51236375+Stanchevdev@users.noreply.github.com>
2020-03-09 15:42:38 +0200

    Update MainClass.java

--- HOMEWORK9/MainClass.java
+++ HOMEWORK9/MainClass.java
@@ -2,6 +2,10 @@ package fmi.informatics.HOMEWORK9;
 
 public class MainClass extends StreamClass{
     public static void main(String[] args) {
+        taskOne();
+        taskTwo();
+        taskThree();
+        taskFour();
         taskFive();
     }
 }
